fed593d542f0df8e4b061a1bf1022309bf705b84
KimJinSeong-Git <acmiheee@gmail.com>
2020-05-19 22:51:17 +0900

    temp

---
+++ 10000-10999/no10996_star21.java
@@ -0,0 +1,24 @@
+/*
+< 문제 >
+ - 예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+
+< 입력 >
+ - 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+< 출력 >
+ - 첫째 줄부터 차례대로 별을 출력한다.
+*/
+import java.util.Scanner;
+public class no10996_star21 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);        
+        int N;
+
+        N = scan.nextInt();
+        for(int i=0; i<N; i++){
+            for(int j=0; j<2; j++){
+
+            }
+        }
+    }
+}
